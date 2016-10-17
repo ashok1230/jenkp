3cb6a505b29f53fe51bc3b3bd81f2977cf940cdc
Ashok Raju <gadiashok0410@gmail.com>
2016-10-17 22:41:24 +0530

    Added teacher model

--- jenka/models.py
+++ jenka/models.py
@@ -6,6 +6,6 @@ class Student(models.Model):
    name = models.CharField(max_length=30)
    standard = models.IntegerField()
 
-class Teachers(models.Model):
+class Teacher(models.Model):
    name = models.CharField(max_length=30)
    subject = models.CharField(max_length=20)
